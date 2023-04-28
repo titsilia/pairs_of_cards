0455182ae716ff95fe8454be185fcc5d814db1dc
elik <elik@example.com>
2023-04-28 20:07:49 +0500

    add lesson 3

--- script.js
+++ script.js
@@ -1,21 +1,119 @@
-const form = document.querySelector('.complexity');
+const main = document.querySelector(".main__firstcreen");
+const form = document.querySelector(".complexity");
 
-const inputs = document.querySelectorAll('.complexity__inputs');
+const inputs = document.querySelectorAll(".complexity__inputs");
 console.log(inputs);
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-        const target = event.target;
-        console.log(target);
+    const target = event.target;
+    console.log(target);
 
-        const complexityInput = document.querySelector('.complexity__inputs:checked');
-        console.log(complexityInput);
+    const complexityInput = document.querySelector(
+        ".complexity__inputs:checked"
+    );
+    console.log(complexityInput);
 
-        const complexityType = complexityInput.value;
-        console.log(complexityType);
+    const complexityType = complexityInput.value;
+    console.log(complexityType);
 
+    localStorage.setItem("complexity", complexityType);
+    console.log(localStorage);
+    renderScreen();
+});
 
-        localStorage.setItem('complexity', complexityType);
-        console.log(localStorage);
-    });
+function renderScreen() {
+    main.remove();
+
+    function templateCards() {
+        return {
+            tag: "section",
+            cls: ["main", "center"],
+            content: [
+                {
+                    tag: "div",
+                    cls: "top",
+                    content: [
+                        {
+                            tag: "div",
+                            cls: "top__times",
+                            content: [
+                                {
+                                    tag: "div",
+                                    cls: "top__texts",
+                                    content: [
+                                        {
+                                            tag: "p",
+                                            cls: "top__texts_text",
+                                            content: "min",
+                                        },
+                                        {
+                                            tag: "p",
+                                            cls: "top__texts_text",
+                                            content: "sek",
+                                        },
+                                    ],
+                                },
+                                {
+                                    tag: "p",
+                                    cls: "top__time",
+                                    content: "00.00",
+                                },
+                            ],
+                        },
+                        {
+                            tag: "button",
+                            cls: ["top__button", "buttons"],
+                            content: "Начать заново",
+                        },
+                    ],
+                },
+                {
+                    tag: "div",
+                    cls: "cards",
+                },
+            ],
+        };
+    }
+
+    const tempCards = templateEngine(templateCards());
+    document.body.appendChild(tempCards);
+
+    const cards = document.querySelector(".cards");
+
+    if (localStorage.getItem("complexity") === "lowlvl") {
+        for (let i = 1; i <= 6; i++) {
+            renderCards();
+        }
+        setTimeout(reverseCards, 3000);
+    }
+
+    if (localStorage.getItem("complexity") === "middlelvl") {
+        for (let i = 1; i <= 12; i++) {
+            renderCards();
+        }
+        setTimeout(reverseCards, 3000);
+    }
+
+    if (localStorage.getItem("complexity") === "highlvl") {
+        for (let i = 1; i <= 18; i++) {
+            renderCards();
+        }
+        setTimeout(reverseCards, 3000);
+    }
+
+    function renderCards() {
+        const cardsImg = document.createElement("img");
+        cardsImg.classList.add("cards__img");
+        cardsImg.setAttribute("src", "./img/9c.svg");
+        cards.appendChild(cardsImg);
+    }
+
+    function reverseCards() {
+        const cardsImg = document.querySelectorAll(".cards__img");
+        cardsImg.forEach((img) => {
+            img.setAttribute("src", "./img/rubashka.svg");
+        });
+    }
+}
