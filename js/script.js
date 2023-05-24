fa23f5c0f26d40dcd9acb1c35948acd0e31ee13e
elik <elik@example.com>
2023-05-24 16:13:06 +0500

    add all files lesson-4

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import "../css/style.css";
 const main = document.querySelector(".main__firstcreen");
 const form = document.querySelector(".complexity");
 
-const button = document.querySelector('.complexity__button');
+const button = document.querySelector(".complexity__button");
 
 // запись выбранного значения в local
 form.addEventListener("submit", (event) => {
@@ -17,7 +17,7 @@ form.addEventListener("submit", (event) => {
     );
 
     // что делать, если пользователь выбрал сложность
-    if(complexityInput) {
+    if (complexityInput) {
         const complexityType = complexityInput.value;
 
         localStorage.setItem("complexity", complexityType);
@@ -25,12 +25,9 @@ form.addEventListener("submit", (event) => {
         renderScreen();
     } else {
         // что делать, если не выбрал
-        alert('Сначала выберите значение!');
+        alert("Сначала выберите значение!");
     }
-    
 });
-
-
 
 // отрисовка элементов второго экрана игры
 function renderScreen() {
@@ -90,19 +87,10 @@ function renderScreen() {
     const tempCards = templateEngine(templateCards());
     document.body.appendChild(tempCards);
 
+    const mainPlay = document.querySelector(".main");
+
     const cards = document.querySelector(".cards");
-    const buttonReplay = document.querySelector('.top__button');
-
-    buttonReplay.addEventListener('click', (event) => {
-        while (cards.firstChild) {
-            cards.removeChild(cards.firstChild);
-          }
-
-        for (let i = 1; i <= 2; i++) {
-            renderCards();
-        }
-        setTimeout(reverseCards, 3000);
-    });
+    const buttonReplay = document.querySelector(".top__button");
 
     // создаём массив с картинками карт
     const CARDS = [
@@ -144,27 +132,42 @@ function renderScreen() {
         "/static/img/Qs.svg",
     ];
 
-
-
-// записываем количество карт, которые нужно отрисовать при опр. уровне 
+    // записываем количество карт, которые нужно отрисовать при опр. уровне
     const level = {
         lowlvl: 6,
         middlelvl: 12,
         highlvl: 18,
     };
 
+    // функционал кнопки Начать заново
+    buttonReplay.addEventListener("click", (event) => {
+        while (cards.firstChild) {
+            cards.removeChild(cards.firstChild);
+        }
 
-// вызов функции получения массива карт
+        for (let i = 1; i <= 2; i++) {
+            renderCards();
+        }
+        setTimeout(reverseCards, 5000);
+    });
+
+    // массив для готовых карт
+    let imgCards = [];
+
+    // массив выбранных карт
+    let resultCards = [];
+
+    // вызов функции получения массива карт
     gerArrayCards();
 
-// отрисовка всех карт 2 раза и переворот
+    // отрисовка всех карт 2 раза и переворот
     for (let i = 1; i <= 2; i++) {
         renderCards();
     }
     setTimeout(reverseCards, 3000);
+    console.log(imgCards);
 
-
-// функция для получения массива карт
+    // функция для получения массива карт
     function gerArrayCards() {
         shuffleCards(CARDS);
 
@@ -180,8 +183,7 @@ function renderScreen() {
         window.cardsRenders = cardsRender;
     }
 
-
-// функция отрисовки карт
+    // функция отрисовки карт
     function renderCards() {
         shuffleCards(window.cardsRenders);
 
@@ -192,13 +194,15 @@ function renderScreen() {
 
             // добавляем в элемент картинку
             cardsImg.setAttribute("src", card);
+            cardsImg.setAttribute("id", card);
+
+            imgCards.push(cardsImg);
 
             cards.appendChild(cardsImg);
         });
     }
 
-
-// функция переворачивания карт
+    // функция переворачивания карт
     function reverseCards() {
         const cardsImg = document.querySelectorAll(".cards__img");
         cardsImg.forEach((img) => {
@@ -206,12 +210,110 @@ function renderScreen() {
         });
     }
 
-// функция перемешивания массива
+    // функция перемешивания массива
     function shuffleCards(array) {
         array.sort(() => Math.random() - 0.5);
     }
 
+    // логика переворачивания и сравнения карт
 
- // логика переворачивания и сравнения карт
+    // вызов функции по клику
+    cards.addEventListener("click", (event) => {
+        const target = event.target;
 
+        handleClickCards(target);
+    });
+
+    // функция записи значения карт в массив
+    function handleClickCards(target) {
+        target.setAttribute("src", `${target.id}`);
+
+        if (!window.firstCard) {
+            window.firstCard = target.id;
+            console.log(window.firstCard);
+        } else {
+            window.secondCart = target.id;
+            console.log(window.secondCart);
+        }
+
+        gameResult();
+    }
+
+    // функция вывода результата
+    function gameResult() {
+        console.log("запуск функции");
+        if (window.firstCard && window.secondCart) {
+            console.log("запуск сравнения");
+            if (window.firstCard === window.secondCart) {
+                console.log("они равны");
+                resultCards.push(firstCard);
+                resultCards.push(secondCart);
+
+                if (resultCards.length === imgCards.length) {
+                    console.log("экран выигрыша");
+                    window.resultImg = "/static/img/win.svg";
+                    window.textResult = "Вы выиграли!";
+                    renderResultScreen();
+                }
+            } else {
+                console.log("экран проигрыша!");
+                window.resultImg = "/static/img/lose.svg";
+                window.textResult = "Вы проиграли!";
+                renderResultScreen();
+            }
+
+            window.firstCard = null;
+            window.secondCart = null;
+        }
+    }
+
+    // экран конца игры
+    function renderResultScreen() {
+        function templateResult() {
+            return {
+                tag: "div",
+                cls: "result",
+                content: [
+                    {
+                        tag: "img",
+                        cls: "result__img",
+                        attrs: {
+                            src: window.resultImg,
+                        },
+                    },
+                    {
+                        tag: "p",
+                        cls: "result__title",
+                        content: window.textResult,
+                    },
+                    {
+                        tag: "p",
+                        cls: "result__text",
+                        content: "Затраченное время:",
+                    },
+                    {
+                        tag: "p",
+                        cls: "result__time",
+                        content: "1.20",
+                    },
+                    {
+                        tag: "button",
+                        cls: ["top__button", "buttons", "btn_reload"],
+                        content: "Играть снова",
+                    },
+                ],
+            };
+        }
+
+        const tempResult = templateEngine(templateResult());
+        document.body.appendChild(tempResult);
+
+        mainPlay.classList.add("opacity");
+        buttonReplay.setAttribute("disabled", "disabled");
+
+        const reloadButton = document.querySelector(".btn_reload");
+        reloadButton.addEventListener("click", (event) => {
+            location.reload();
+        });
+    }
 }
