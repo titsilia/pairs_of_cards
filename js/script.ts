1283517c4294bee28a14387854b57e0bf8759cf8
NoMoreNap <vv2trash@gmail.com>
2023-05-29 17:21:32 +0300

    helped with ts

--- js/script.ts
+++ js/script.ts
@@ -7,6 +7,17 @@ const form = document.querySelector(".complexity")!;
 
 const button = document.querySelector(".complexity__button")!;
 
+window.application = {
+    cardsRenders: [],
+    firstCard: undefined,
+    secondCart: undefined,
+    resultImg: undefined,
+    textResult: undefined,
+
+
+
+}
+
 // запись выбранного значения в local
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -174,9 +185,6 @@ function renderScreen() {
     setTimeout(reverseCards, 3000);
     console.log(imgCards);
 
-    window.application = {
-        cardsRenders: [],
-    };
 
     // функция для получения массива карт
     function gerArrayCards() {
