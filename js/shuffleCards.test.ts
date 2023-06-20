3e9420ceb3591a7559e1e5bd9b56d0f115654ef4
elik <elik@example.com>
2023-06-20 13:21:52 +0500

    added test

--- js/shuffleCards.test.ts
+++ js/shuffleCards.test.ts
@@ -1,3 +1,16 @@
-const { it } = require("@jest/globals");
-const assert = require("assert").strict;
 import { shuffleCards } from "./shuffleCards";
+import { it, expect, describe } from "@jest/globals";
+
+describe("Не пропадают ли значения", () => {
+    it("shuffles and checks array length", () => {
+        const strings = ["1", "2", "3", "4"];
+        shuffleCards(strings);
+        expect(strings).toHaveLength(4);
+        expect(strings).not.toHaveLength(6);
+    });
+    it("additionaly", () => {
+        const strings = ["1", "2", "3", "4", "5"];
+        shuffleCards(strings);
+        expect(strings).not.toHaveLength(2);
+    });
+});
